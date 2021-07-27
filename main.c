53ab3ac86052f6ba7bb8f929dfe294141a9fea0f
mateusxa <mateus_xa@hotmail.com>
2021-07-27 20:41:28 -0300

    CLOCKinit
    Created a function to setup the main clock.
    In this case the STM will run on internal clock (HSI - 8 MHz) with no prescaler.

--- main.c
+++ main.c
@@ -1,10 +1,39 @@
 
 #include "stm32f030x6.h"
 
+
+#define PLL	2
+
+
+void CLOCKinit(void);
+
 int main (void){
 
+	
 	while(1){
 		
 	}
+	
+}
+
+void CLOCKinit(void){
+	
+	// ENABLE HSI and wait for the HSE to become Ready
+	RCC->CR |= RCC_CR_HSION;
+	while(!(RCC->CR & RCC_CR_HSIRDY));
+	
+	// Set the POWER ENABLE CLOCK and VOLTAGE REGULATOR
+	RCC->APB1ENR |= RCC_APB1ENR_PWREN;
+
+	// Configure the PRESCALARS HCLK, PLCK1, PLCK2
+	// AHB PR
+	RCC->CFGR &= RCC_CFGR_HPRE_0;
+	
+	// APB PR
+	RCC->CFGR |= RCC_CFGR_PPRE_0;
+	
+	// Set Internal Clock as clock source
+	RCC->CFGR |= RCC_CFGR_SW_HSI;
+	while((RCC->CFGR & RCC_CFGR_SWS) != RCC_CFGR_SWS_HSI);
 	
 }
