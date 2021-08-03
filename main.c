1a2baa551cd187e7b60efc190b29afe82c8fa18a
Mateus Xavier <mateus_xa@hotmail.com>
2021-08-03 20:18:31 -0300

    Fixed Clockinit

--- main.c
+++ main.c
@@ -14,9 +14,9 @@ int main (void){
 	
 	while(1){
 		GPIOA->BSRR |= (1 << 3);		// Set PA14
-		delay(100000000);
+		delay(1000000);
 		GPIOA->BSRR |= ((1 << 3) << 16);		// Reset PA14
-		delay(10000000);
+		delay(1000000);
 	}
 	
 }
@@ -34,26 +34,16 @@ void CLOCKinit(void){
 	RCC->CR |= RCC_CR_HSION;
 	while(!(RCC->CR & RCC_CR_HSIRDY));
 	
-	// Set the POWER ENABLE CLOCK and VOLTAGE REGULATOR
-	RCC->APB1ENR |= RCC_APB1ENR_PWREN;
-
-	// Configure the PRESCALARS HCLK, PLCK1, PLCK2
-	// AHB PR
-	RCC->CFGR &= RCC_CFGR_HPRE_0;
+	RCC->CFGR |= RCC_CFGR_HPRE_DIV1; // NUCLEO RODANDO 8MHZ
 	
-	// APB PR
-	RCC->CFGR |= RCC_CFGR_PPRE_0;
-	
-	// Set Internal Clock as clock source
-	RCC->CFGR |= RCC_CFGR_SW_HSI;
-	while((RCC->CFGR & RCC_CFGR_SWS) != RCC_CFGR_SWS_HSI);
+	RCC->CFGR |= RCC_CFGR_PPRE_DIV8;	// PERIFERICOS RODANDO A 1MHZ
 	
 }
 
 void GPIOinit(void){
 
 	// Enable GPIO Port A Clock
-	RCC->AHBENR |= RCC_AHBENR_GPIOAEN;
+	RCC->AHBENR |= RCC_AHBENR_GPIOAEN;	
 	
 	// Setting PA14 as output
 	GPIOA->MODER |= (1 << 6); // BITS 29:28 -> 01
@@ -61,5 +51,10 @@ void GPIOinit(void){
 	
 }
 void delay (uint32_t time){
-	while(time--);
+	do{
+		__NOP();
+		__NOP();
+		__NOP();
+		__NOP();
+	}while(time--);
 }
